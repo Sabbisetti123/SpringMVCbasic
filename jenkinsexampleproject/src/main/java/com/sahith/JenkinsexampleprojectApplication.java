5d97935fd8e3e5ef40b96144e119d3d8c56503a1
Sabbisetti123 <sahithsabbisetti@gmail.com>
2022-09-19 15:29:35 +0530

    done

---
+++ jenkinsexampleproject/src/main/java/com/sahith/JenkinsexampleprojectApplication.java
@@ -0,0 +1,14 @@
+package com.sahith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsexampleprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsexampleprojectApplication.class, args);
+		System.out.println("THis is example one");
+	}
+
+}
---
+++ jenkinsexampleproject/src/test/java/com/sahith/JenkinsexampleprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsexampleprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
